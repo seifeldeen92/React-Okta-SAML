fc3e26c1dd137be1f63bff7a2446cc203ab33815
seifeldeen92 <s.khaled_92@hotmail.com>
2021-04-16 05:59:24 +0200

    add: server config for express saml session

---
+++ server/src/config/config.ts
@@ -0,0 +1,23 @@
+import { EXPRESS_PORT } from "./constants";
+const config = {
+  saml: {
+    cert: "./src/config/saml.pem",
+    entryPoint:
+      "https://dev-27121687.okta.com/app/dev-27121687_reactsaml_1/exkla36xuusmmbk6e5d6/sso/saml",
+    issuer: "http://localhost:5000",
+    options: {
+      faliureFlash: true,
+      failureRedirect: "/login",
+    },
+  },
+  server: {
+    port: EXPRESS_PORT,
+  },
+  session: {
+    saveUnitialized: true,
+    resave: false,
+    secret: "secret",
+  },
+};
+
+export default config;
