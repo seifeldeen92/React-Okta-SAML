72da8dcd15f7cd439dbca68ad7046507efbee7f9
seifeldeen92 <s.khaled_92@hotmail.com>
2021-04-16 05:49:01 +0200

    add: auth logic in the ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import logging from "./config/logging";
+import axios from "axios";
+const App: React.FunctionComponent = (props) => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [email, setEmail] = useState<string>("");
 
-const App = () => <div>Hey, you are not authenticated by Okta!</div>;
+  useEffect(() => {
+    logging.info("check authentication");
+
+    axios
+      .get("http://localhost:5000/me", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        if (res.data.user.nameID) {
+          setEmail(res.data.user.nameID);
+          setLoading(false);
+        } else {
+          redirectToLogin();
+        }
+      })
+      .catch((err) => {
+        logging.error(err, "saml");
+        setLoading(false);
+        redirectToLogin();
+      });
+  }, []);
+
+  const redirectToLogin = () => {
+    window.location.replace("http://localhost:5000/login");
+  };
+
+  if (loading) return <p>loading...</p>;
+
+  return <p>Woho! you are authenticated via Okta. Your email is ${email}</p>;
+};
 
 export default App;
