0cc684936e833b0251c054a9856889e1fd8b1b27
seifeldeen92 <s.khaled_92@hotmail.com>
2021-04-16 07:00:32 +0200

    refactor: app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from "react";
 import logging from "./config/logging";
 import axios from "axios";
-const App: React.FunctionComponent = (props) => {
+
+const App: React.FunctionComponent = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [email, setEmail] = useState<string>("");
 
   useEffect(() => {
-    logging.info("check authentication");
+    logging.info("checking authentication by calling /me");
 
     axios
       .get("http://localhost:5000/me", {
         withCredentials: true,
       })
       .then((res) => {
+        logging.info(res.data.user, "/me");
+
+        /**
+         * if user has no nameID it means that he is not authenticated yet!
+         * redirect to login for Okta auth
+         */
         if (res.data.user.nameID) {
           setEmail(res.data.user.nameID);
           setLoading(false);
@@ -21,19 +28,18 @@ const App: React.FunctionComponent = (props) => {
         }
       })
       .catch((err) => {
-        logging.error(err, "saml");
-        setLoading(false);
+        logging.error(err, "/me");
         redirectToLogin();
       });
   }, []);
 
+  // Redirect to login
   const redirectToLogin = () => {
     window.location.replace("http://localhost:5000/login");
   };
 
   if (loading) return <p>loading...</p>;
-
-  return <p>Woho! you are authenticated via Okta. Your email is ${email}</p>;
+  return <p>Woho! {email} is now authenticated via Okta.</p>;
 };
 
 export default App;
